533793ec5ff03bf096ea588a93df395c3d6b2af4
Nick Buk[0vec] <nbukovec@calpoly.edu>
2023-08-21 16:36:10 -0700

    submit + reset

--- components/support-form.tsx
+++ components/support-form.tsx
@@ -15,6 +15,7 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
+import { useState } from "react"
 
 const phoneNumberSchema = z
   .union([z.string().min(10).max(10), z.string().length(0)])
@@ -31,6 +32,8 @@ const formSchema = z.object({
   phone: phoneNumberSchema,
 })
 export const SupportForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [success, setSuccess] = useState(false)
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -41,77 +44,99 @@ export const SupportForm = () => {
   })
 
   const onSubmit = (values: z.infer<typeof formSchema>) => {
-    console.log(values)
+    setIsSubmitting(true)
+    setSuccess(true)
+    setIsSubmitting(false)
   }
 
-  return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full max-w-2xl space-y-8"
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <div className="flex flex-row items-center gap-1">
+  const reset = () => {
+    form.reset()
+    setSuccess(false)
+  }
+
+  if (!success) {
+    return (
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full max-w-2xl space-y-8"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <div className="flex flex-row items-center gap-1">
+                    <Input
+                      className="max-w-2xl"
+                      placeholder="mustymustang"
+                      {...field}
+                    />
+                    <p className="text-sm text-muted-foreground">@calpoly.edu</p>
+                  </div>
+                </FormControl>
+                <FormDescription>
+                  The first part of your Cal Poly email address
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="summary"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Summary</FormLabel>
+                <FormControl>
                   <Input
                     className="max-w-2xl"
-                    placeholder="mustymustang"
+                    placeholder="I need help with..."
                     {...field}
                   />
-                  <p className="text-sm text-muted-foreground">@calpoly.edu</p>
-                </div>
-              </FormControl>
-              <FormDescription>
-                The first part of your Cal Poly email address
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="summary"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Summary</FormLabel>
-              <FormControl>
-                <Input
-                  className="max-w-2xl"
-                  placeholder="I need help with..."
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>A brief summary of your issue</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="phone"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Phone Number</FormLabel>
-              <FormControl>
-                <Input
-                  type="tel"
-                  className="max-w-2xl"
-                  placeholder="8057565555"
-                  {...field}
-                />
-              </FormControl>
-              <FormDescription>Your phone number</FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+                </FormControl>
+                <FormDescription>A brief summary of your issue</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input
+                    type="tel"
+                    className="max-w-2xl"
+                    placeholder="8057565555"
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>Your phone number</FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    )
+  }
+  return (
+    <div className="flex flex-col items-center gap-2">
+      <h2 className="text-xl font-semibold tracking-tighter md:text-4xl">
+        Thank you!
+      </h2>
+      <p className="text-lg text-center">
+        We will assist you as soon as possible
+      </p>
+      <Button onClick={() => reset()}>Submit another ticket</Button>
+    </div>
   )
+
+
 }
